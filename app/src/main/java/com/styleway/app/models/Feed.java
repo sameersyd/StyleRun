8ec412f7a7953336413eb5519688e30f210a55c3
Sameer Nawaz <sameer.nwaz@gmail.com>
2019-08-02 12:17:46 +0530

    With NewsFeed

--- app/src/main/java/com/styleway/app/models/Feed.java
+++ app/src/main/java/com/styleway/app/models/Feed.java
@@ -1,4 +1,31 @@
 package com.styleway.app.models;
 
 public class Feed {
+
+    String imageUrl,msg;
+
+    public Feed() {}
+
+
+    public Feed(String imageUrl, String msg) {
+        this.imageUrl = imageUrl;
+        this.msg = msg;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
 }
